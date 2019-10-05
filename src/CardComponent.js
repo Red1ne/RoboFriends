5a2cdddb988699ee6369b5240c7644c86c2cd4e5
Red1ne <ranjan.abhishek@protonmail.com>
2019-10-05 20:54:22 +0530

    RoboFriends App

---
+++ src/CardComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card'
+const CardComponent = ( { robots } ) => {
+    return (
+        robots.map((user, i) => {
+            return (<Card 
+                key={i} 
+                id={robots[i].id} 
+                name={robots[i].name} 
+                email={robots[i].email}/>);
+        })
+    );
+}
+
+export default CardComponent;
